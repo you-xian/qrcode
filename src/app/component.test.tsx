59be023cd305e87b27434f0114d491fc2649bb04
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2021-05-13 21:15:17 -0400

    Expect more component

--- src/app/component.test.tsx
+++ src/app/component.test.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 
 import Header from "../component/header";
 import App from "./index";
+import { GlobalStyle } from "./style";
 
 describe('<App />', () => {
   let wrapper: ShallowWrapper;
@@ -19,5 +20,8 @@ describe('<App />', () => {
   test('It should mount following components', () => {
     expect(wrapper.find(ThemeProvider)).toHaveLength(1);
     expect(wrapper.find(Header)).toHaveLength(1);
+    expect(wrapper.find(GlobalStyle)).toHaveLength(1);
+    expect(wrapper.find("img")).toHaveLength(1);
+    expect(wrapper.find("div")).toHaveLength(1);
   })
 })
